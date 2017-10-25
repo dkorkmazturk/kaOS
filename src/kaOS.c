5119a9ec9724090cf541e6eef182fe6456bebafe
Dogukan Korkmazturk <d.korkmazturk@gmail.com>
2017-10-25 11:50:18 +0300

    Suspend and Sleep functions are added

--- src/kaOS.c
+++ src/kaOS.c
@@ -1,7 +1,9 @@
+#include <stdlib.h>
 #include "kaos.h"
 #include "semaphore.h"
 
 tcb_t* RunPt = NULL;
+tcb_t* WaitPt = 0;
 
 void SVCaller(void *param0, void* param1, void* param2, const uint8_t SVCall_Number) {
     if(SVCall_Number == 0)
@@ -10,6 +12,73 @@ void SVCaller(void *param0, void* param1, void* param2, const uint8_t SVCall_Num
         sem_signal((sem_t*)param0);
     else if(SVCall_Number == 2)
         sem_wait((sem_t*)param0);
+    else if(SVCall_Number == 3) {
+        __asm__ volatile("CPSID I");
+        NVIC_ST_CURRENT_R = NVIC_ST_RELOAD_R;
+        NVIC_INT_CTRL_R |= NVIC_INT_CTRL_PEND_SV;
+        __asm__ volatile("CPSIE I");
+    }
+}
+
+static void Timer0_Init(const uint32_t period)
+{
+    SYSCTL_RCGCTIMER_R |= SYSCTL_RCGCTIMER_R0;
+
+    NVIC_PRI4_R = (NVIC_PRI4_R & 0x00FFFFFF) | 0x20000000;
+
+    TIMER0_CTL_R = 0;   // Disable Timer0
+    TIMER0_CFG_R = 0;   // 32-bit timer
+    TIMER0_TAMR_R = 0x2;    // Periodic-mode
+    TIMER0_TAILR_R = period;    // Period
+    TIMER0_IMR_R = 1;   // Timeouts will trigger interrupt
+
+    NVIC_EN0_R |= (1 << 19);    // Enable 19th IRQ
+}
+
+void Timer0_Handler(void)
+{
+    // Timer0 interrupt handler
+    // Priority 2
+
+    TIMER0_ICR_R = 1;   // Acknowledge the interrupt
+
+    tcb_t* waitingNodePrev = NULL;
+    tcb_t* waitingNode = WaitPt;
+
+    while(waitingNode != NULL)
+    {
+        --(waitingNode->waitTime);
+
+        if(waitingNode->waitTime == 0)
+        {
+            if(waitingNode != WaitPt)
+                waitingNodePrev->previous = waitingNode->previous;
+            else
+            {
+                WaitPt = WaitPt->previous;
+                if(WaitPt == NULL)
+                {
+                    TIMER0_CTL_R &= ~1; // If there is no remaining process that is waiting, it is pointless to use timer, so disable it
+                    TIMER0_TAV_R = TIMER0_TAILR_R;
+                }
+            }
+
+            waitingNode->next = RunPt->next;
+            waitingNode->previous = RunPt;
+            RunPt->next = waitingNode;
+            waitingNode->next->previous = waitingNode;
+
+            if(waitingNodePrev != NULL)
+                waitingNode = waitingNodePrev->previous;
+            else
+                waitingNode = WaitPt;
+        }
+        else
+        {
+            waitingNodePrev = waitingNode;
+            waitingNode = waitingNode->previous;
+        }
+    }
 }
 
 static void Watchdog_Timer0_Init(void)
@@ -35,6 +104,7 @@ static void SysTick_Init(void)
     NVIC_ST_CTRL_R = 0;
     NVIC_ST_RELOAD_R = 0x00FFFFFF;
     NVIC_ST_CURRENT_R = 0;
+    NVIC_SYS_PRI3_R = (NVIC_SYS_PRI3_R & 0x00FFFFFF) | 0x20000000;
 }
 
 void SysTick_Start(void)
@@ -44,19 +114,25 @@ void SysTick_Start(void)
 
 void SysTick_Handler(void)
 {
+    // SysTick timer interrupt handler
+    // Priority 2
+
     Watchdog_Timer0_Restart();
     NVIC_INT_CTRL_R |= NVIC_INT_CTRL_PEND_SV;
 }
 
 void Scheduler(void) {
+    if(RunPt == NULL)
+        __asm__ volatile("WFI");
+
     RunPt = RunPt->next;
-    __asm__("MOV R1, #0xFFFFFFFF");
 }
 
 void kaOS_Init(void)
 {
     SysTick_Init();
     Watchdog_Timer0_Init();
+    Timer0_Init(0x00FFFFFF);
 }
 
 int8_t kaOS_AddThead(void (*thread)(void))
@@ -84,6 +160,7 @@ int8_t kaOS_AddThead(void (*thread)(void))
     newThread->stack[STACKSIZE - 16] = 0x04040404; // R4
 
     newThread->sp = &(newThread->stack[STACKSIZE - 16]);
+    newThread->waitTime = 0;
 
     if(RunPt == NULL)
     {
@@ -116,4 +193,34 @@ void kaOS_SemSignal(sem_t* const sem)
 void kaOS_SemWait(sem_t* const sem)
 {
     __asm__ volatile("SVC 2");
+}
+
+void kaOS_Suspend(void)
+{
+    __asm__ volatile("SVC 3");
+}
+
+void kaOS_Sleep(const uint32_t ms)
+{
+    RunPt->previous->next = RunPt->next;
+    RunPt->next->previous = RunPt->previous;
+    RunPt->previous = NULL;
+    RunPt->waitTime = ms;
+
+    if(WaitPt == NULL)
+    {
+        WaitPt = RunPt;
+        TIMER0_CTL_R |= 1;   // Start Timer0
+    }
+    else
+    {
+        tcb_t* node = WaitPt;
+
+        while(node->previous != NULL)
+            node = node->previous;
+
+        node->previous = RunPt;
+    }
+
+    kaOS_Suspend();
 }
