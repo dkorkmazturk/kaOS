4523b274cc19c391095c6332a0c776c047569ef6
Dogukan Korkmazturk <d.korkmazturk@gmail.com>
2017-10-30 16:53:02 +0300

    GPIO drivers are added

---
+++ src/drivers/gpio.h
@@ -0,0 +1,17 @@
+/*
+ * gpio.h
+ *
+ * This file contains the declarations of GPIO driver functions
+ */
+
+#ifndef GPIO_H_
+#define GPIO_H_
+
+#include "types.h"
+
+void GPIO_Init(const enum GPIO_PORT port);
+void GPIO_SetMode(const enum GPIO_PORT port, const uint32_t pins, const enum GPIO_MODE mode);
+void GPIO_Write(const enum GPIO_PORT port, const uint32_t pins, const uint32_t values);
+inline uint32_t GPIO_Read(const enum GPIO_PORT port, const uint32_t pins) {return *((&GPIO_PORTA_DATA_R) + port) & pins;}
+
+#endif /* GPIO_H_ */
